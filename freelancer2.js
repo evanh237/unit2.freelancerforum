bb3acd319f5ac7b8393ec25d4f3d69bebd7f6e98
evanh237 <evanh237@gmail.com>
2023-12-05 18:44:19 -0600

    homework updated

--- freelancer2.js
+++ freelancer2.js
@@ -4,41 +4,76 @@ const freelancers = [
   { name: "Carol", job: "Programmer", price: 70 },
 ];
 
+const priceValue = document.querySelector("#priceValue");
+
 function createTableHeader(table) {
   const thead = document.createElement("thead");
   const headerRow = document.createElement("tr");
   const headers = ["Name", "Job", "Salary"];
 
-  for (let i = 0; i < headers.length; i++) {
+  for (const header of headers) {
     const th = document.createElement("th");
-    const header = headers[i];
+
     th.innerText = header;
     headerRow.appendChild(th);
   }
   thead.appendChild(headerRow);
   table.appendChild(thead);
 }
+
 function createTableBody(table, freelancers) {
   const tbody = document.createElement("tbody");
-  for (let i = 0; i < freelancers.length; i++) {
-    const row = document.createElement("tr");
-    const freelancer = freelancers[i];
+  for (const freelancer of freelancers) {
+    const tableRow = document.createElement("tr");
+
     for (const key in freelancer) {
       const cell = document.createElement("td");
       cell.innerText = freelancer[key];
-      row.appendChild(cell);
+      tableRow.appendChild(cell);
     }
+    tbody.appendChild(tableRow);
   }
-  tbody.appendChild(row);
-  table.appendchild(tbody);
+  table.appendChild(tbody);
 }
 
 function appendTableToContainer(table) {
   const root = document.querySelector("#root");
-  const h1 = document.createElement("h1");
-  h1.innertext = "Freelancer Database";
-  root.appendchild(h1);
+  root.innerHTML = "";
+  const h2 = document.createElement("h2");
+  h2.innertext = "Available Freelancers";
+  root.appendChild(h2);
   root.appendChild(table);
+}
+
+function updateTableAndAverage(newFreelancer) {
+  freelancers.push(newFreelancer);
+  const table = document.createElement("table");
+  createTableBody(table, freelancers);
+  appendTableToContainer(table);
+  calculateAverage(freelancers);
+}
+
+function addNewFreelancer() {
+  const intervalId =
+    (() => {
+      const newFreelancer = {
+        name: "Carol",
+        job: "Programmer",
+        price: 70,
+      };
+      updateTableAndAverage(newFreelancer);
+    },
+    3000);
+  return intervalId;
+}
+
+function calculateAverage(freelancers) {
+  const totalPrice = freelancers.reduce((prev, curr) => {
+    const price = prev + curr.price;
+    return price;
+  }, 0);
+  const averagePrice = totalPrice / freelancers.length;
+  priceValue.innerText = Math.floor(averagePrice);
 }
 
 function render(initialState) {
@@ -46,6 +81,12 @@ function render(initialState) {
   createTableHeader(table);
   createTableBody(table, initialState);
   appendTableToContainer(table);
+  calculateAverage(freelancers);
+  addNewFreelancer();
 }
+const intervalId = addNewFreelancer();
+setTimeout(() => {
+  clearInterval(intervalId);
+}, 5000);
 
 render(freelancers);
